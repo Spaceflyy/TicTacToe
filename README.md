d6a125620b769529aa9af5bd22baf9592b061e01
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-04-06 21:03:21 +0100

    Add ability to place pieces on board

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TicTacToe
+
+Game of Tic Tac Toe to pracise basic modules and factory functions
--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello");
+const gameManager = () => {};
+
+const displayManager = () => {
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  let board = document.querySelectorAll(".cell");
+
+  const addSymbol = (e) => {
+    gameBoard[e.target.getAttribute("data")] = "X";
+    updateBoard();
+  };
+  board.forEach((element) => {
+    element.addEventListener("click", addSymbol);
+  });
+
+  const updateBoard = () => {
+    let currentIndex = 0;
+    board.forEach((element) => {
+      element.innerText = gameBoard[currentIndex];
+      currentIndex++;
+    });
+  };
+};
+
+const Player = (name, symbol) => {};
+
+const test = displayManager();
