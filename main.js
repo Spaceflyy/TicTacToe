fb9a1fa7eba5138e4cb9772d6d6a16b638e6b60c
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-04-07 16:51:33 +0100

    Add ability to place each players peices on the grid

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-const gameManager = () => {};
-
-const displayManager = () => {
+const displayManager = (() => {
   let gameBoard = ["", "", "", "", "", "", "", "", ""];
   let board = document.querySelectorAll(".cell");
+  let currentSymbol;
+  let currentTurnDisplay = document.getElementById("turnDisplay");
+
+  const getBoard = () => {
+    return { gameBoard };
+  };
 
   const addSymbol = (e) => {
-    gameBoard[e.target.getAttribute("data")] = "X";
-    updateBoard();
+    if (e.target.innerText == "") {
+      if (player1.turn == true) {
+        currentTurnDisplay.innerText = "Player 2's Turn";
+        currentSymbol = player1.symbol;
+        player1.turn = false;
+        player2.turn = true;
+      } else {
+        currentTurnDisplay.innerText = "Player 1's Turn";
+        currentSymbol = player2.symbol;
+        player2.turn = false;
+        player1.turn = true;
+      }
+      gameBoard[e.target.getAttribute("data")] = currentSymbol;
+      updateBoard();
+    }
   };
+
   board.forEach((element) => {
     element.addEventListener("click", addSymbol);
   });
@@ -19,8 +37,17 @@ const displayManager = () => {
       currentIndex++;
     });
   };
+
+  return { getBoard };
+})();
+
+const Player = (symbol, turn) => {
+  return { symbol, turn };
 };
 
-const Player = (name, symbol) => {};
+const gameManager = (() => {
+  // displayManager.getBoard();
+})();
 
-const test = displayManager();
+const player1 = Player("X", true);
+const player2 = Player("O", false);
